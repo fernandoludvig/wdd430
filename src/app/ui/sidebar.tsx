2727d56e9bb07a5cc7d4e86a9dd499417f43b52d
Fernando Ludvig <fernandoludvig0804@gmail.com>
2025-11-08 18:07:42 -0300

    Add NavLinks component for dashboard sidebar

--- src/app/ui/sidebar.tsx
+++ src/app/ui/sidebar.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { clsx } from "clsx";
-
-const links = [
-  { name: "Home", href: "/dashboard" },
-  { name: "Invoices", href: "/dashboard/invoices" },
-  { name: "Customers", href: "/dashboard/customers" },
-];
+import NavLinks from "./nav-links";
 
 export default function Sidebar() {
   return (
@@ -22,41 +15,10 @@ export default function Sidebar() {
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        {links.map((link) => {
-          return (
-            <NavLink key={link.name} href={link.href}>
-              {link.name}
-            </NavLink>
-          );
-        })}
+        <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
       </div>
     </div>
-  );
-}
-
-function NavLink({
-  href,
-  children,
-}: {
-  href: string;
-  children: React.ReactNode;
-}) {
-  const pathname = usePathname();
-  const isActive = pathname === href;
-
-  return (
-    <Link
-      href={href}
-      className={clsx(
-        "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 focus:bg-sky-100 focus:text-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 md:flex-none md:justify-start md:p-2 md:px-3",
-        {
-          "bg-sky-100 text-blue-600": isActive,
-        }
-      )}
-    >
-      {children}
-    </Link>
   );
 }
 
